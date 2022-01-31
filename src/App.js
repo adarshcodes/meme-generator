f2876c03841afcc1a112d2b432635b78110ffe4c
Adarshcodes <adarsh.lrnr@gmail.com>
2022-01-31 17:29:30 +0530

    completed static form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Main from "./Components/Main";
 import "./Sass/main.css";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<Main />
 		</div>
 	);
 }
---
+++ src/Components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./Form";
+
+export default class Main extends React.Component {
+	render() {
+		return (
+			<div className="main">
+				<Form />
+			</div>
+		);
+	}
+}
