716f0e9357451baab85d74b8059b82ecb0824e97
Adarshcodes <adarsh.lrnr@gmail.com>
2022-02-01 17:42:39 +0530

    fetching images using button

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Form from "./Form";
 import Meme from "./Meme";
 
 export default class Main extends React.Component {
 	render() {
 		return (
 			<div className="main">
-				<Form />
 				<Meme />
 			</div>
 		);
--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,17 +1,49 @@
 import React from "react";
+import memeData from "../memeData";
 
-class Meme extends React.Component {
-	render() {
-		return (
-			<div className="meme-container">
-				<img
-					src="https://cms.qz.com/wp-content/uploads/2018/07/meme-featured.jpg?quality=75&strip=all&w=900&h=900&crop=1"
-					alt="meme"
-					className="meme-container__image"
-				/>
-			</div>
-		);
+function Meme() {
+	const [memeImage, setMemeImage] = React.useState(
+		"http://i.imgflip.com/1bij.jpg"
+	);
+
+	function getMemeImage() {
+		const memesArray = memeData.data.memes;
+		const randomNumber = Math.floor(Math.random() * memesArray.length);
+		setMemeImage(memesArray[randomNumber].url);
 	}
+
+	return (
+		<>
+			<div className="form-container">
+				<div action="#" className="form-container--form">
+					<div className="form-container--form__inputs">
+						<input
+							type="text"
+							className="form-container--form__input"
+							id="top-text"
+							name="top-text"
+							placeholder="Top Text"
+						/>
+
+						<input
+							type="text"
+							className="form-container--form__input"
+							id="bottom-text"
+							name="bottom-text"
+							placeholder="Bottom Text"
+						/>
+					</div>
+
+					<button className="form-container__button" onClick={getMemeImage}>
+						Get a new meme image 🖼
+					</button>
+				</div>
+			</div>
+			<div className="meme-container">
+				<img src={memeImage} alt="meme" className="meme-container__image" />
+			</div>
+		</>
+	);
 }
 
 export default Meme;
