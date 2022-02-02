41aa3bb6a1c0d1517484aeb98a3cd821794183f8
Adarshcodes <adarsh.lrnr@gmail.com>
2022-02-02 21:04:38 +0530

    relearning state concept

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import Form from "./Form";
 import Meme from "./Meme";
 
-export default class Main extends React.Component {
-	render() {
-		return (
-			<div className="main">
-				<Meme />
-			</div>
-		);
-	}
+export default function Main() {
+	return (
+		<div className="main">
+			<Form />
+			<Meme />
+		</div>
+	);
 }
--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,61 +1,14 @@
 import React from "react";
-import memeData from "../memeData";
 
 function Meme() {
-	const [meme, setMeme] = React.useState({
-		topText: "",
-		bottomText: "",
-		randomImage: "http://i.imgflip.com/1bij.jpg",
-	});
-
-	const [allMemeImages, setAllMemeImages] = React.useState(memeData);
-
-	function getMemeImage() {
-		const memesArray = allMemeImages.data.memes;
-		const randomNumber = Math.floor(Math.random() * memesArray.length);
-		const url = memesArray[randomNumber].url;
-
-		setMeme((prevMeme) => ({
-			...prevMeme,
-			randomImage: url,
-		}));
-	}
-
 	return (
-		<>
-			<div className="form-container">
-				<div action="#" className="form-container--form">
-					<div className="form-container--form__inputs">
-						<input
-							type="text"
-							className="form-container--form__input"
-							id="top-text"
-							name="top-text"
-							placeholder="Top Text"
-						/>
-
-						<input
-							type="text"
-							className="form-container--form__input"
-							id="bottom-text"
-							name="bottom-text"
-							placeholder="Bottom Text"
-						/>
-					</div>
-
-					<button className="form-container__button" onClick={getMemeImage}>
-						Get a new meme image 🖼
-					</button>
-				</div>
-			</div>
-			<div className="meme-container">
-				<img
-					src={meme.randomImage}
-					alt="meme"
-					className="meme-container__image"
-				/>
-			</div>
-		</>
+		<div className="meme-container">
+			<img
+				src="https://i.imgflip.com/25w3.jpg"
+				alt="meme"
+				className="meme-container__image"
+			/>
+		</div>
 	);
 }
 
